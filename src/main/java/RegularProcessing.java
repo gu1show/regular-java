c9abd5138cee58b62465719e7f22a8afd7c2b187
gu1show <denissolovyov57@gmail.com>
2022-09-17 23:31:06 +0300

    Add the ability to check if a string is an Ip address

---
+++ src/main/java/RegularProcessing.java
@@ -0,0 +1,11 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularProcessing {
+    public boolean isIpAddress(String input){
+        String filter = "^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$";
+        Pattern pattern = Pattern.compile(filter);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+}
---
+++ src/test/java/RegularProcessingTest.java
@@ -0,0 +1,82 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegularProcessingTest {
+    @Test
+    public void testIsIpAddressCorrect1(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertTrue(executor.isIpAddress("127.0.0.1"));
+    }
+
+    @Test
+    public void testIsIpAddressCorrect2(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertTrue(executor.isIpAddress("255.255.255.255"));
+    }
+
+    @Test
+    public void testIsIpAddressCorrect3(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertTrue(executor.isIpAddress("0.0.0.0"));
+    }
+
+    @Test
+    public void testIsIpAddressCorrect4(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertTrue(executor.isIpAddress("1.10.255.128"));
+    }
+
+    @Test
+    public void testIsIpAddressCorrect5(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertTrue(executor.isIpAddress("20.0.5.1"));
+    }
+
+    @Test
+    public void testIsIpAddressCorrect6(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertTrue(executor.isIpAddress("18.100.29.138"));
+    }
+
+    @Test
+    public void testIsIpAddressIncorrect1(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertFalse(executor.isIpAddress(" 18.100.29.138"));
+    }
+
+    @Test
+    public void testIsIpAddressIncorrect2(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertFalse(executor.isIpAddress("18.100.29.138 "));
+    }
+
+    @Test
+    public void testIsIpAddressIncorrect3(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertFalse(executor.isIpAddress("256.34.29.138"));
+    }
+
+    @Test
+    public void testIsIpAddressIncorrect4(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertFalse(executor.isIpAddress("183..0.81"));
+    }
+
+    @Test
+    public void testIsIpAddressIncorrect5(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertFalse(executor.isIpAddress("25 75 2 130"));
+    }
+
+    @Test
+    public void testIsIpAddressIncorrect6(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertFalse(executor.isIpAddress("18..0.98.46"));
+    }
+
+    @Test
+    public void testIsIpAddressIncorrect7(){
+        RegularProcessing executor = new RegularProcessing();
+        Assert.assertFalse(executor.isIpAddress("18.01.98.100"));
+    }
+}
